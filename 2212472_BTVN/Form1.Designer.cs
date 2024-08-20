7a090da7141ad36c2965aa38125db97d2bd126f5
PC826 <PC826@PC826>
2024-08-20 08:48:28 +0700

    Add project files.

---
+++ 2212472_BTVN/Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace GuessNumberGame
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.TextBox txtGuess;
+        private System.Windows.Forms.Button btnGuess;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.txtGuess = new System.Windows.Forms.TextBox();
+            this.btnGuess = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Location = new System.Drawing.Point(13, 13);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(189, 13);
+            this.lblMessage.TabIndex = 0;
+            this.lblMessage.Text = "Nhập số từ 1 đến 100 và nhấn \'Đoán\'!";
+            this.lblMessage.Click += new System.EventHandler(this.lblMessage_Click);
+            // 
+            // txtGuess
+            // 
+            this.txtGuess.Location = new System.Drawing.Point(16, 39);
+            this.txtGuess.Multiline = true;
+            this.txtGuess.Name = "txtGuess";
+            this.txtGuess.Size = new System.Drawing.Size(100, 20);
+            this.txtGuess.TabIndex = 1;
+            this.txtGuess.Text = "50";
+            this.txtGuess.TextChanged += new System.EventHandler(this.txtGuess_TextChanged);
+            // 
+            // btnGuess
+            // 
+            this.btnGuess.Location = new System.Drawing.Point(122, 37);
+            this.btnGuess.Name = "btnGuess";
+            this.btnGuess.Size = new System.Drawing.Size(75, 23);
+            this.btnGuess.TabIndex = 2;
+            this.btnGuess.Text = "Đoán";
+            this.btnGuess.UseVisualStyleBackColor = true;
+            this.btnGuess.Click += new System.EventHandler(this.btnGuess_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 81);
+            this.Controls.Add(this.btnGuess);
+            this.Controls.Add(this.txtGuess);
+            this.Controls.Add(this.lblMessage);
+            this.Name = "Form1";
+            this.Text = "Trò Chơi Đoán Số";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+    }
+}
---
+++ 2212472_BTVN/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace GuessNumberGame
+{
+    public partial class Form1 : Form
+    {
+        private int targetNumber;
+        private int attempts;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeGame();
+        }
+
+        private void InitializeGame()
+        {
+            // Khởi tạo số ngẫu nhiên và số lần thử
+            Random random = new Random();
+            targetNumber = random.Next(1, 101); // Số từ 1 đến 100
+            attempts = 0;
+            lblMessage.Text = "Nhập số từ 1 đến 100 và nhấn 'Đoán'!";
+        }
+
+        private void btnGuess_Click(object sender, EventArgs e)
+        {
+            int guessedNumber;
+            if (int.TryParse(txtGuess.Text, out guessedNumber))
+            {
+                attempts++;
+                if (guessedNumber < targetNumber)
+                {
+                    lblMessage.Text = "Số đoán thấp hơn. Thử lại!";
+                }
+                else if (guessedNumber > targetNumber)
+                {
+                    lblMessage.Text = "Số đoán cao hơn. Thử lại!";
+                }
+                else
+                {
+                    lblMessage.Text = $"Chúc mừng! Bạn đã đoán đúng số {targetNumber} sau {attempts} lần thử.";
+                    // Bắt đầu lại trò chơi
+                    InitializeGame();
+                }
+            }
+            else
+            {
+                lblMessage.Text = "Vui lòng nhập một số hợp lệ!";
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InitializeGame();
+        }
+
+        private void txtGuess_TextChanged(object sender, EventArgs e)
+        {
+         
+        }
+
+        private void lblMessage_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
